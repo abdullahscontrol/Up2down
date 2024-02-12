e029cb4c14908b1b85b4080d8f33386c01c43112
abdullah <mariyahassangaya12@gmail.com>
2024-02-12 13:28:00 +0100

    djfnsd

--- src/header.jsx
+++ src/header.jsx
@@ -8,7 +8,7 @@ const someData = [
 function Header() {
     return (
         <div className="koyona">
-            <h1>Up2down<span id="quest">!</span></h1>
+            <h1>Up2down<span id="quest"> !</span></h1>
             <div className="course-name">
                 Download and read
             </div>
