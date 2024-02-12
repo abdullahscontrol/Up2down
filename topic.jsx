d083225394cde73739d9ec1b9b0504d2dfb70f7f
abdullah <mariyahassangaya12@gmail.com>
2024-02-12 13:25:14 +0100

    Some

--- topic.jsx
+++ topic.jsx
@@ -1,3 +1,13 @@
+const edgeData =
+{
+
+}
+const readData = [];
+
+
+
+
+
 import Header from "./src/header";
 function Topic() {
     return (
@@ -5,12 +15,15 @@ function Topic() {
             <Edge />
             <Edge />
             <Edge />
+            <Read />
+            <Read />
+            <Read />
         </div>
     )
 }
 function Edge() {
     return (
-        <div >
+        <div>
             <div className="edge">
                 <video controls >
                     <source src="banana.mp4" type="video/mp4" />
@@ -27,9 +40,26 @@ function Edge() {
                     <br />
                 </div> */}
             </div>
-        </div >
+        </div>
     )
 }
 // export default Edge;
-
+function Read() {
+    return (
+        <div className="read">
+            <div className="read-heading">  Temakawa Iyaye</div>
+            <div className="read-media">
+                <img src="001-m.PNG" alt="" id="read-media" />
+            </div>
+            <div className="read-article">
+                "Ka dubafa kai daya ka kula da yarnka. cinsu, shansu,
+                tufatarwarsu, karatunsu su kusan goma 10 amma idan
+                tsufa yazo maka su sukasa kula da kai wai sai sunyi
+                karo-karo  kaikuma ba wanda yayi
+                karo-karo lokacin da kake kula dasu, wannan yana cikin
+                wulakancin da yara ke yiwa iyayensu."
+            </div>
+        </div>
+    )
+}
 export default Topic;
