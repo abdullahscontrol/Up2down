26655c6a60e176836d33773873ff2fa73311b9c6
Abdullahs Control <144876004+abdullahscontrol@users.noreply.github.com>
2024-03-12 14:18:11 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ const courseData = [
     {
         image: "006-img.jpg",
         title: "Amfanin Karas",
-        article: "Aljazar: wato karas \n Yana daga ciki jinsin tsirai mabankwasa kasa, Bangaren da ake amfani dashi a jinsa shine jijiyarsa wadda take cikin kasa ana iya shuka shi sau biyu a shekara yana da launi mabambanta. Akwai mai riwan dowara-dorawa da kuma ja dakuma fari mai haske Amma duk sanda karas yayi launin ja to shine yake nuna cewa ya nuna sannan kuma yana kunshe da sinadarin carotene wanda jikin mutum yake da bukatarsa a kullum daga awo 1 zuwa 5 na milligram. Dandanon karas mai zaki ne da kuma kayatarwa ganyensa kuma ana tabbatar dace wa abinci ne mai gina jiki ga dabbobi haka nan kuma idan Sinadaran karin lafiya da karas ya kunsa.  1- Vitamin A, 2- Carotene, 3- Carbohydrate, 4- Vitamin E, 5- Vitamin C, 6- Vitamin B6, 7- Potassium, 8- Sodium, 9- Calcium. Ana shuka karas a wajen da yake kasa mara nauyi ko wadda ba ta chakudu da duwarwatsu ba sannan ba a shuka shin a kasar da bata da hasken rana sosai. Babban Malami Dawud Al-Andaki yace:Kwayoyin irin karas sunfi karas din amfani a jikin Dan Adam (Tazkiratu Ulul Albab); Fa`idojin Karas. 1- Idan aka dafa ganyensa aka sha yana wanke koda ya kar mata lafiya, 2- Yana kasha Kwayoyin cuta dake cikin hanji, 3-Idan aka nika karas aka matse ruwansa aka sha yana maganin kuragen da ke cikin hanji mutun, 4- Yana karawa fatar mutum lafiya tare da magance zafin hasken rana da ke cutar da fata, 5- Karas na junshe da wasu sinadarn da suke daidai ta sukari a jikin dan Adam, 6- Karas na magance tsutar ciki da magwas sannan karas yana maganin sanyi  da tari, 7- Yana taimakawa wajen narkar da abinci da wuri a cikin dan Adam 8- Yana hana zafin ciki tare da wanke mafitsara da matsarmama 9- Yana maganin ciwon anta idan ana shan ruwansa da yawa, 11- Shan ruwan karas da yawa yana magance ciwon daji (Cancer) domin yana kunshe da sinadarin Vitamin A masu yawa, 12- Yana wanke jinni tare da maganin hawan jini"    
+        article: <Article />,
     }, 
         {
         image: "001-img.jpg",
@@ -75,6 +75,38 @@ function Course(Props) {
             <img src={Props.courseObj.image} alt="" width={"100%"} />
             <h1>{Props.courseObj.title}</h1>
             <p>{Props.courseObj.article}</p>
+        </div>
+    )
+}
+export function Article() {
+    return (
+        <div className="article">
+            <h3>Aljazar: wato karas </h3>
+            <p>Yana daga cikin jinsin tsirai mabankwasa kasa, Bangaren da ake amfani dashi a jikinsa shine jijiyarsa wadda take cikin kasa ana iya shuka shi sau biyu a shekara yana da launi mabambanta
+                Akwai mai ruwan dowara-dorawa da kuma ja dakuma fari mai haske Amma duk sanda karas yayi launin ja to shine yake nuna cewa ya nuna sannan kuma yana kunshe da sinadarin carotene wanda jikin mutum yake da bukatarsa a kullum daga awa 1 zuwa 5 na milligram.
+                Dan  dandanon karas mai zaki ne da kuma kayatarwa, ganyensa kuma ana tabbatar dace wa abinci ne mai gina jiki ga dabbobi haka nan kuma idan
+                Sinadaran Karin lafiya da karas ya kunsa
+            </p>
+            <ul>
+                <li>Vitamin A</li>
+                <li>Carotene</li>
+                <li>Carbohydrate A</li>
+                <li>Vitamin E</li>
+                <li>Vitamin C</li>
+                <li>Vitamin B6</li>
+                <li>Pottasium</li>
+                <li>Sodium</li>
+                <li>Calcium</li>
+            </ul>
+            <p>Ana shuka karas a wajen da yake kasa mara nauyi ko wadda ba ta chakudu da duwarwatsu ba sannan ba a shuka shi a kasar da bata da hasken rana sosai.
+                Babban Malami Dawud Al-Andaki yace: Kwayoyin irin karas sunfi karas din amfani  ajikin dan Adam(Tazkiratu Ulul Albab);
+            </p>
+            <h3>Fa`idojin Karas </h3>
+            <ul>
+                <li>Idan aka dafa ganyensa aka sha yana wanke koda ya kara mata lafiya </li>
+                <li>Yana kasha kwayoyin cuta dake cikin hanji  </li>
+                <li>Idan aka nika karasa aka matse ruwansa aka sha yana maganin kuragen da ke cikin hanji mutum </li>
+            </ul>
         </div>
     )
 }
