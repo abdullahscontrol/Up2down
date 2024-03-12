778ec1d41768c4489a68404b473415473f34b27f
Abdullahs Control <144876004+abdullahscontrol@users.noreply.github.com>
2024-03-12 14:20:57 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,63 +4,7 @@ const courseData = [
         title: "Amfanin Karas",
         article: <Article />,
     }, 
-        {
-        image: "001-img.jpg",
-        title: "Amfanin Tumatir",
-        article: "Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },    {
-        image: "001-img.jpg",
-        title: "Amfanin Tumatir",
-        article: "Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },    {
-        image: "001-img.jpg",
-        title: "Amfanin Tumatir",
-        article: "Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },    {
-        image: "001-img.jpg",
-        title: "Amfanin Tumatir",
-        article: "Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },
-    {
-        image: "002-img.jpg",
-        title: "Zo kaci Karas",
-        article: " Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },
-    {
-        image: "003-img.jpg",
-        title: "Hasken Tumaturi",
-        article: " Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },
-    {
-        image: "004-img.jpg",
-        title: "Falalar Tumaturi",
-        article: " Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },
-    {
-        image: "005-img.jpg",
-        title: "Alkaras",
-        article: " Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
-    },
-    {
-        image: "006-img.jpg",
-        title: "Karassss",
-        article: " Tumatir dai mahadi ne sananne ga mutane masu dama a mabambantan abinci, kodai a dafashi, ko a soya, ko ma aci danye, yana da kaloli daban-daban, amma wanda aka fi sani shi ja, sai dai akwai ruwan lemo, rawaya har ma da ruwan malmo, qwargwadon qasashen da suke amfani da shi, a taqaice dai, wasu..."
 
-    },
-     {
-        image: "006-img.jpg",
-        title: "Amfanin Karas",
-        article: "Aljazar: wato karas \n Yana daga ciki jinsin tsirai mabankwasa kasa, Bangaren da ake amfani dashi a jinsa shine jijiyarsa wadda take cikin kasa ana iya shuka shi sau biyu a shekara yana da launi mabambanta. Akwai mai riwan dowara-dorawa da kuma ja dakuma fari mai haske Amma duk sanda karas yayi launin ja to shine yake nuna cewa ya nuna sannan kuma yana kunshe da sinadarin carotene wanda jikin mutum yake da bukatarsa a kullum daga awo 1 zuwa 5 na milligram. Dandanon karas mai zaki ne da kuma kayatarwa ganyensa kuma ana tabbatar dace wa abinci ne mai gina jiki ga dabbobi haka nan kuma idan Sinadaran karin lafiya da karas ya kunsa.  1- Vitamin A, 2- Carotene, 3- Carbohydrate, 4- Vitamin E, 5- Vitamin C, 6- Vitamin B6, 7- Potassium, 8- Sodium, 9- Calcium. Ana shuka karas a wajen da yake kasa mara nauyi ko wadda ba ta chakudu da duwarwatsu ba sannan ba a shuka shin a kasar da bata da hasken rana sosai. Babban Malami Dawud Al-Andaki yace:Kwayoyin irin karas sunfi karas din amfani a jikin Dan Adam (Tazkiratu Ulul Albab); Fa`idojin Karas. 1- Idan aka dafa ganyensa aka sha yana wanke koda ya kar mata lafiya, 2- Yana kasha Kwayoyin cuta dake cikin hanji, 3-Idan aka nika karas aka matse ruwansa aka sha yana maganin kuragen da ke cikin hanji mutun, 4- Yana karawa fatar mutum lafiya tare da magance zafin hasken rana da ke cutar da fata, 5- Karas na junshe da wasu sinadarn da suke daidai ta sukari a jikin dan Adam, 6- Karas na magance tsutar ciki da magwas sannan karas yana maganin sanyi  da tari, 7- Yana taimakawa wajen narkar da abinci da wuri a cikin dan Adam 8- Yana hana zafin ciki tare da wanke mafitsara da matsarmama 9- Yana maganin ciwon anta idan ana shan ruwansa da yawa, 11- Shan ruwan karas da yawa yana magance ciwon daji (Cancer) domin yana kunshe da sinadarin Vitamin A masu yawa, 12- Yana wanke jinni tare da maganin hawan jini"    
-    }, 
-     {
-        image: "006-img.jpg",
-        title: "Amfanin Karas",
-        article: "Aljazar: wato karas \n Yana daga ciki jinsin tsirai mabankwasa kasa, Bangaren da ake amfani dashi a jinsa shine jijiyarsa wadda take cikin kasa ana iya shuka shi sau biyu a shekara yana da launi mabambanta. Akwai mai riwan dowara-dorawa da kuma ja dakuma fari mai haske Amma duk sanda karas yayi launin ja to shine yake nuna cewa ya nuna sannan kuma yana kunshe da sinadarin carotene wanda jikin mutum yake da bukatarsa a kullum daga awo 1 zuwa 5 na milligram. Dandanon karas mai zaki ne da kuma kayatarwa ganyensa kuma ana tabbatar dace wa abinci ne mai gina jiki ga dabbobi haka nan kuma idan Sinadaran karin lafiya da karas ya kunsa.  1- Vitamin A, 2- Carotene, 3- Carbohydrate, 4- Vitamin E, 5- Vitamin C, 6- Vitamin B6, 7- Potassium, 8- Sodium, 9- Calcium. Ana shuka karas a wajen da yake kasa mara nauyi ko wadda ba ta chakudu da duwarwatsu ba sannan ba a shuka shin a kasar da bata da hasken rana sosai. Babban Malami Dawud Al-Andaki yace:Kwayoyin irin karas sunfi karas din amfani a jikin Dan Adam (Tazkiratu Ulul Albab); Fa`idojin Karas. 1- Idan aka dafa ganyensa aka sha yana wanke koda ya kar mata lafiya, 2- Yana kasha Kwayoyin cuta dake cikin hanji, 3-Idan aka nika karas aka matse ruwansa aka sha yana maganin kuragen da ke cikin hanji mutun, 4- Yana karawa fatar mutum lafiya tare da magance zafin hasken rana da ke cutar da fata, 5- Karas na junshe da wasu sinadarn da suke daidai ta sukari a jikin dan Adam, 6- Karas na magance tsutar ciki da magwas sannan karas yana maganin sanyi  da tari, 7- Yana taimakawa wajen narkar da abinci da wuri a cikin dan Adam 8- Yana hana zafin ciki tare da wanke mafitsara da matsarmama 9- Yana maganin ciwon anta idan ana shan ruwansa da yawa, 11- Shan ruwan karas da yawa yana magance ciwon daji (Cancer) domin yana kunshe da sinadarin Vitamin A masu yawa, 12- Yana wanke jinni tare da maganin hawan jini"    
-    },  {
-        image: "006-img.jpg",
-        title: "Amfanin Karas",
-        article: "Aljazar: wato karas \n Yana daga ciki jinsin tsirai mabankwasa kasa, Bangaren da ake amfani dashi a jinsa shine jijiyarsa wadda take cikin kasa ana iya shuka shi sau biyu a shekara yana da launi mabambanta. Akwai mai riwan dowara-dorawa da kuma ja dakuma fari mai haske Amma duk sanda karas yayi launin ja to shine yake nuna cewa ya nuna sannan kuma yana kunshe da sinadarin carotene wanda jikin mutum yake da bukatarsa a kullum daga awo 1 zuwa 5 na milligram. Dandanon karas mai zaki ne da kuma kayatarwa ganyensa kuma ana tabbatar dace wa abinci ne mai gina jiki ga dabbobi haka nan kuma idan Sinadaran karin lafiya da karas ya kunsa.  1- Vitamin A, 2- Carotene, 3- Carbohydrate, 4- Vitamin E, 5- Vitamin C, 6- Vitamin B6, 7- Potassium, 8- Sodium, 9- Calcium. Ana shuka karas a wajen da yake kasa mara nauyi ko wadda ba ta chakudu da duwarwatsu ba sannan ba a shuka shin a kasar da bata da hasken rana sosai. Babban Malami Dawud Al-Andaki yace:Kwayoyin irin karas sunfi karas din amfani a jikin Dan Adam (Tazkiratu Ulul Albab); Fa`idojin Karas. 1- Idan aka dafa ganyensa aka sha yana wanke koda ya kar mata lafiya, 2- Yana kasha Kwayoyin cuta dake cikin hanji, 3-Idan aka nika karas aka matse ruwansa aka sha yana maganin kuragen da ke cikin hanji mutun, 4- Yana karawa fatar mutum lafiya tare da magance zafin hasken rana da ke cutar da fata, 5- Karas na junshe da wasu sinadarn da suke daidai ta sukari a jikin dan Adam, 6- Karas na magance tsutar ciki da magwas sannan karas yana maganin sanyi  da tari, 7- Yana taimakawa wajen narkar da abinci da wuri a cikin dan Adam 8- Yana hana zafin ciki tare da wanke mafitsara da matsarmama 9- Yana maganin ciwon anta idan ana shan ruwansa da yawa, 11- Shan ruwan karas da yawa yana magance ciwon daji (Cancer) domin yana kunshe da sinadarin Vitamin A masu yawa, 12- Yana wanke jinni tare da maganin hawan jini"    
-    }
 ];
 function Advertise() { 
     return (
